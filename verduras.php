49643c1c11a0946c58fee4bcf06e364429b9ad12
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:26:36 -0300

    Create verduras.php

---
+++ verduras.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <title>verduras</title>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">verduras</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="menu.php">menu</a></li>
+        <li><a href="carrinho.php">carrinho</a></li>
+      </ul>
+    </div>
+  </nav>
+  <img src="imagensdoprojeto\alho.png" width="40" height="40">
+  <p>alho KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+  <br>
+  <img src="imagensdoprojeto\alhoporó.jpg" width="40" height="40">
+  <p>alho poró KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\brocolis.jpg" width="40" height="40">
+  <p>brócolis KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\pimentao.jpg" width="40" height="40">
+  <p>pimentão KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\cebola.jpg" width="40" height="40">
+  <p>cebola KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+</body>
+</html>
