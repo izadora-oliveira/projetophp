7817c28c6551cf5f3955520b4328466bbc90a511
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:23:38 -0300

    Create folhagens.php

---
+++ folhagens.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <title>folhagens</title>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">folhagens </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="menu.php">menu</a></li>
+        <li><a href="carrinho.php">carrinho</a></li>
+      </ul>
+    </div>
+  </nav>
+  <img src="imagensdoprojeto\alface.png" width="40" height="40">
+  <p>alface KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+  <br>
+  <img src="imagensdoprojeto\acelgachinesa.jpg" width="40" height="40">
+  <p>acelgachinesa KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\cebolinha.jpg" width="40" height="40">
+  <p>cebolinha KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\coentro.jpg" width="40" height="40">
+  <p>coentro KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\rucula.png" width="40" height="40">
+  <p>rucula KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+</body>
+</html>
