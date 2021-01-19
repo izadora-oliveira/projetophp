dc70d222ec5d7ae1fb60ae777a85d0972f750f08
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:23:41 -0300

    Create frutas.php

---
+++ frutas.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <title>frutas</title>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">frutas </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="menu.php">menu</a></li>
+        <li><a href="carrinho.php">carrinho</a></li>
+      </ul>
+    </div>
+  </nav>
+  <img src="imagensdoprojeto\banana.png" width="40" height="40">
+  <p>banana KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+  <br>
+  <img src="imagensdoprojeto\laranja.jpg" width="40" height="40">
+  <p>laranja KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\manga.jpg" width="40" height="40">
+  <p>manga KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\morango.jpg" width="40" height="40">
+  <p>morango KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\goiaba.jpg" width="40" height="40">
+  <p>goiaba KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+</body>
+</html>
