3fec20c4d1c944f16cd5a23dc12f94b29c4f1c6a
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:23:31 -0300

    Create carrinho.php

---
+++ carrinho.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>carrinho</title>
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">carrinho </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="javascript:history.back()">voltar</a></li>
+        <li><a href="menu.php">menu</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
