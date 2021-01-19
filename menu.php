6461bff2ddbdb5bf11f0bb07acd2bc6a8ea965eb
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:26:25 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>menu</title>
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Menu </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="entrar.php">Entrar</a></li>
+        <li><a href="cadastro.php">cadastrar</a></li>
+      </ul>
+    </div>
+  </nav>
+<div class="collection">
+        <a href="frutas.php" class="collection-item">frutas</a>
+        <a href="verduras.php" class="collection-item active">verduras</a>
+        <a href="folhagens.php" class="collection-item">folhagens</a>
+        <a href="raizes.php" class="collection-item">raízes</a>
+      </div>
+</body>
+</html>
