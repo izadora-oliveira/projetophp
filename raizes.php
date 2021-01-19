f942aee12fc8934716542cdfbf2a37f951fe1c3b
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:26:32 -0300

    Create raizes.php

---
+++ raizes.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <title>raizes</title>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">raizes </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="menu.php">menu</a></li>
+        <li><a href="carrinho.php">carrinho</a></li>
+      </ul>
+    </div>
+  </nav>
+  <img src="imagensdoprojeto\cará.jpg" width="40" height="40">
+  <p>cara KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+  <br>
+  <img src="imagensdoprojeto\inhame.jpg" width="40" height="40">
+  <p>inhame KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\jerimun.jpg" width="40" height="40">
+  <p>jerimun KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\mandioca.jpg" width="40" height="40">
+  <p>mandioca KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+
+  <br>
+  <img src="imagensdoprojeto\cenoura.jpg" width="40" height="40">
+  <p>cenoura KG</p>
+  <p>R$10</p>
+  <p>
+    <input name="produto" type="text">
+  </p>
+  <button>Comprar</button>
+</body>
+</html>
