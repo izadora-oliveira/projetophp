016a54e54073f59c744efb5db9b09c2d9469c403
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:23:22 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>cadastro</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+        <a href="#" class="brand-logo">cadastro </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="menu.php">menu</a></li>
+            <li><a href="entrar.php">entrar</a></li>
+        </ul>
+        </div>
+    </nav>
+    <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Nome</label>
+        </div>
+      </div>
+    <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+    <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Senha</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">repetir senha</label>
+        </div>
+      </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <input id="endereço" type="text" class="validate">
+            <label for="endereço">endereço</label>
+            </div>
+        </div>
+      
+</body>
+</html>
