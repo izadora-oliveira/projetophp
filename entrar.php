6de99145526f4d767ecd351541b7b2de7b53be53
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-18 22:23:35 -0300

    Create entrar.php

---
+++ entrar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Entrar</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+        <a href="#" class="brand-logo">entrar </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="menu.php">menu</a></li>
+            <li><a href="cadastro.php">cadastrar</a></li>
+        </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+    <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">senha</label>
+        </div>
+      </div>
+      
+</body>
+</html>
