6d4c6449f15974905361cc85c4929f34e3bdb498
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-20 23:00:20 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +12,6 @@
         <div class="nav-wrapper">
         <a href="#" class="brand-logo">cadastro </a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="menu.php">menu</a></li>
             <li><a href="entrar.php">entrar</a></li>
         </ul>
         </div>
@@ -47,6 +46,7 @@
             <label for="endereço">endereço</label>
             </div>
         </div>
+    <a class="btn btn-primary" href="entrar.php" role="button">Cadastrar</a>
       
 </body>
 </html>
