09a343bcc3740aa5daf97e7bbd4981302f9b7e4d
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-28 18:44:08 -0300

    Update folhagens.php

--- folhagens.php
+++ folhagens.php
@@ -12,52 +12,25 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">folhagens </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="menu.php">menu</a></li>
+        <li><a href="index.php">menu</a></li>
         <li><a href="carrinho.php">carrinho</a></li>
       </ul>
+      
     </div>
   </nav>
-  <img src="imagensdoprojeto\alface.png" width="40" height="40">
-  <p>alface KG</p>
-  <p>R$10</p>
-  <p>
-    <input name="produto" type="text">
-  </p>
-  <button>Comprar</button>
-  <br>
-  <img src="imagensdoprojeto\acelgachinesa.jpg" width="40" height="40">
-  <p>acelgachinesa KG</p>
-  <p>R$10</p>
-  <p>
-    <input name="produto" type="text">
-  </p>
-  <button>Comprar</button>
-
-  <br>
-  <img src="imagensdoprojeto\cebolinha.jpg" width="40" height="40">
-  <p>cebolinha KG</p>
-  <p>R$10</p>
-  <p>
-    <input name="produto" type="text">
-  </p>
-  <button>Comprar</button>
-
-  <br>
-  <img src="imagensdoprojeto\coentro.jpg" width="40" height="40">
-  <p>coentro KG</p>
-  <p>R$10</p>
-  <p>
-    <input name="produto" type="text">
-  </p>
-  <button>Comprar</button>
-
-  <br>
-  <img src="imagensdoprojeto\rucula.png" width="40" height="40">
-  <p>rucula KG</p>
-  <p>R$10</p>
-  <p>
-    <input name="produto" type="text">
-  </p>
-  <button>Comprar</button>
+  <ul class="collection">
+    <li class="collection-item avatar">
+      <img src="imagensdoprojeto\acelgachinesa.jpg" alt="" class="circle">
+      <div class="row">
+        <div class="col s12">
+          Quantidade:
+          <div class="input-field inline">
+            <input id="email_inline" type="email" class="validate">
+          </div>
+        </div>
+      </div>
+      </p>
+    </li>
+  </ul>
 </body>
 </html>
