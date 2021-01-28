24047422f3ff8a299d3a663f0f629aba9b6ed860
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-01-28 18:44:33 -0300

    Update verduras.php

--- verduras.php
+++ verduras.php
@@ -12,7 +12,7 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">verduras</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="menu.php">menu</a></li>
+        <li><a href="index.php">menu</a></li>
         <li><a href="carrinho.php">carrinho</a></li>
       </ul>
     </div>
