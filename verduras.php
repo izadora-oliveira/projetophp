c3686d0b6cdcb82b305d5eeedee5fcd0e45d9dba
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-02-04 22:24:39 -0300

    Update verduras.php

--- verduras.php
+++ verduras.php
@@ -8,6 +8,9 @@
   <title>verduras</title>
 </head>
 <body>
+  <?php
+  require_once("autenticar.php")
+  ?>
   <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">verduras</a>
