954efb7ac760d56a76fc371150fc9b6b8e563245
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-02-15 22:58:38 -0300

    Update folhagens.php

--- folhagens.php
+++ folhagens.php
@@ -23,48 +23,51 @@
       
     </div>
   </nav>
-  <table>
-        <thead>
-          <tr>
-              <th>Item</th>
-              <th>Nome</th>
-              <th>Preço</th>
-              <th>Quantidade</th>
-          </tr>
-        </thead>
+  <form  action = "add-to-cart.php" method = "POST">
+    <table>
+          <thead>
+            <tr>
+                <th>Item</th>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td><img src="imagensdoprojeto\acelgachinesa.jpg" width="40" height="40"></td>
-            <td>Acelga chinesa</td>
-            <td>$2.00</td>
-            <td><input name="acelgachinesa" type="int"><button>Comprar</button></td>
-          </tr>
-          <tr>
-            <td><img src="imagensdoprojeto\alface.png" width="40" height="40"></td>
-            <td>Alface</td>
-            <td>$2.50</td>
-            <td><input name="alface" type="int"><button>Comprar</button></td>
-          </tr>
-          <tr>
-            <td><img src="imagensdoprojeto\coentro.jpg" width="40" height="40"></td>
-            <td>Coentro</td>
-            <td>$1.50</td>
-            <td><input name="coentro" type="int"><button>Comprar</button></td>
-          </tr>
-          <tr>
-            <td><img src="imagensdoprojeto\rucula.png" width="40" height="40"></td>
-            <td>Rúcula</td>
-            <td>$2.00</td>
-            <td><input name="rucula" type="int"><button>Comprar</button></td>
-          </tr>
-          <tr>
-            <td><img src="imagensdoprojeto\cebolinha.jpg" width="40" height="40"></td>
-            <td>Cebolinha</td>
-            <td>$1.50</td>
-            <td><input name="cebolinha" type="int"><button>Comprar</button></td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody>
+            <tr>
+              <td><img src="imagensdoprojeto\acelgachinesa.jpg" width="40" height="40"></td>
+              <td><input type="text" name="item6" value="Acelga chinesa" readonly></td>
+              <td><input type="number" name="preco6" value="2.00" readonly></td>
+              <td><input type="number" name="quantidade6"></td>
+            </tr>              
+            <tr>
+              <td><img src="imagensdoprojeto\alface.png" width="40" height="40"></td>
+              <td><input type="text" name="item7" value="Alface" readonly></td>
+              <td><input type="number" name="preco7" value="2.50" readonly></td>
+              <td><input type="number" name="quantidade7"></td>
+            </tr>
+            <tr>
+              <td><img src="imagensdoprojeto\coentro.jpg" width="40" height="40"></td>
+              <td><input type="text" name="item8" value="Coentro" readonly></td>
+              <td><input type="number" name="preco8" value="1.50" readonly></td>
+              <td><input type="number" name="quantidade8"></td>
+            </tr>
+            <tr>
+              <td><img src="imagensdoprojeto\rucula.png" width="40" height="40"></td>
+              <td><input type="text" name="item9" value="Rúcula" readonly></td>
+              <td><input type="number" name="preco9" value="2.00" readonly></td>
+              <td><input type="number" name="quantidade9"></td>
+            </tr>
+            <tr>
+              <td><img src="imagensdoprojeto\cebolinha.jpg" width="40" height="40"></td>
+              <td><input type="text" name="item10" value="Cebolinha" readonly></td>
+              <td><input type="number" name="preco10" value="1.50" readonly></td>
+              <td><input type="number" name="quantidade10"></td>
+            </tr>
+          </tbody>
+        </table>
+      <input type="submit" value="Adicionar ao carrinho">
+    </form>
 </body>
 </html>
