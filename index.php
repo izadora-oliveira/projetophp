fdcaa179c9b1a51e0f6e76c1f863d7a0691b7c33
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-02-16 23:06:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,17 +15,16 @@ require_once("autenticar.php")
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Menu </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="carrinho.php">carrinho</a></li>
-        <li><a href="entrar.php">entrar</a></li>
+        <li><a href="carrinho.php">Carrinho</a></li>
         <li><a href="logoff.php">Sair</a></li> 
       </ul>
     </div>
   </nav>
 <div class="collection">
-        <a href="frutas.php" class="collection-item">frutas</a>
-        <a href="verduras.php" class="collection-item active">verduras</a>
-        <a href="folhagens.php" class="collection-item">folhagens</a>
-        <a href="raizes.php" class="collection-item">raízes</a>
+        <a href="frutas.php" class="collection-item">Frutas</a>
+        <a href="verduras.php" class="collection-item active">Verduras</a>
+        <a href="folhagens.php" class="collection-item">Folhagens</a>
+        <a href="raizes.php" class="collection-item">Raízes</a>
       </div>
 </body>
 </html>
