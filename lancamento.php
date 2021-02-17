20dbda54479e9b2398508eda9a59db71ea7d6773
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-02-16 23:06:55 -0300

    Update lancamento.php

--- lancamento.php
+++ lancamento.php
@@ -1,17 +1,3 @@
 <?php
    session_start();
-
-echo "<pre>";
-   print_r($_SESSION['cart']);
-echo "</pre>";
-
-foreach ($_SESSION['cart'] as $key => $value) :
-   $query = implode(',', $value);
-   echo "INSERT INTO teste (nome, dinheiro) VALUES ('{$query}')";
-
-endforeach;
-
-foreach ($_SESSION['cart'] as $key => $value) :
-    echo "INSERT INTO teste (nome, dinheiro) VALUES ('" . $value["name"] . "'," . $value["money"] . ")";
- 
- endforeach;
+   $teste = $_SESSION['cart'];
