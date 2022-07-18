5d2e7165fc352ab2c150d721c372e16deaff9724
izadora oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2022-07-17 22:42:17 -0300

    17/07

--- cadastro.php
+++ cadastro.php
@@ -1,39 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php include "header.php" ?>
 <body>
-    <nav>
-        <div class="nav-wrapper">
-        <a href="#" class="brand-logo">cadastro </a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="entrar.php">entrar</a></li>
-        </ul>
-        </div>
-    </nav>
-    <form method="POST" action="cadastrar.php" class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="nome" class = 'validate' required = "" id="name" type="text" class="validate">
-          <label for="name">Nome</label>
+<section class="background-radial-gradient overflow-hidden">
+    <style>
+      .background-radial-gradient {
+        background-image: url("imagens/index.png");
+      }
+      .bg-glass {
+        background-color: hsl(30, 100%, 70%) !important;
+      }
+    </style>
+
+    <div class="container px-4 py-5 px-md-5  my-5">
+      <div class="card bg-glass" style="width: 30rem;">
+        <div class="card-body px-4 py-5 px-md-5">
+        <h1 class="card-title" style="color: hsl(120, 60%, 50%)">Cadastro</h1>
+        <form  action = "cadastrar.php" method = "POST">
+          <!-- Nome input -->
+          <div class="form-outline mb-4">
+            <input type="text" name="nome" id="nome" class="form-control" />
+            <label class="form-label" for="email">Nome</label>
+          </div>
+
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="email" name="email" id="email" class="form-control" />
+            <label class="form-label" for="email">Email</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-4">
+            <input type="password" name="senha" id="senha" class="form-control" />
+            <label class="form-label" for="senha">Senha</label>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="password" name="senha" id="senha" class="form-control" />
+            <label class="form-label" for="senha">Repita Senha</label>
+          </div>
+
+          <!-- Submit button -->
+          <input type="submit" name="entrar" value="Entrar" class="btn btn-success btn-block mb-4"></input>
+          <button type="button" class="btn btn-warning btn-block mb-4">
+          <a href="cadastro.php">Cadastrar</a>
+          </button>
+
+          </form>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="email" class = 'validate' required = "" type="email" class="validate" placeholder="E-mail">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="senha" class = 'validate' required = "" id="password" type="password" class="validate">
-          <label for="password">Senha</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="endereco" class = 'validate' required = "" id="endereço" type="text" class="validate">
-          <label for="endereço">endereço</label>
-        </div>
-      </div>
-      <button class="btn btn-primary " role="button" >Cadastrar</button>
-    </form>
+    </div>
+  </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -10,44 +10,37 @@
         }
         .bg-glass {
           background-color: hsl(30, 100%, 70%) !important;
-          backdrop-filter: saturate(200%) blur(25px);
         }
       </style>
 
-      <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-        <div class="row gx-lg-5 align-center mb-5">
-          <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-            <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(120, 60%, 50%)">
-              Feira Delivery
-            </h1>
-          </div>
-            <div class="card bg-glass" style="width: 30rem;">
-              <div class="card-body px-4 py-5 px-md-5">
-              <form  action = "login.php" method = "POST">
-                  <!-- Email input -->
-                  <div class="form-outline mb-4">
-                    <input type="email" name="email" id="email" class="form-control" />
-                    <label class="form-label" for="email">Email</label>
-                  </div>
-
-                  <!-- Password input -->
-                  <div class="form-outline mb-4">
-                    <input type="password" name="senha" id="senha" class="form-control" />
-                    <label class="form-label" for="senha">Senha</label>
-                  </div>
-
-                  <!-- Submit button -->
-                  <button type="submit" class="btn btn-success btn-block mb-4">
-                    Entrar
-                  </button>                  
-                </form>
+      <div class="container px-4 py-5 px-md-5  my-5">
+        <div class="card bg-glass" style="width: 30rem;">
+          <div class="card-body px-4 py-5 px-md-5">
+          <h1 class="card-title" style="color: hsl(120, 60%, 50%)">Feira Delivery</h1>
+          <form  action = "login.php" method = "POST">
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" name="email" id="email" class="form-control" />
+                <label class="form-label" for="email">Email</label>
               </div>
-            </div>
+
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <input type="password" name="senha" id="senha" class="form-control" />
+                <label class="form-label" for="senha">Senha</label>
+              </div>
+
+              <!-- Submit button -->
+              <input type="submit" name="entrar" value="Entrar" class="btn btn-success btn-block mb-4"></input>
+              <button type="button" class="btn btn-warning btn-block mb-4">
+              <a href="cadastro.php">Cadastrar</a>
+              </button>
+
+            </form>
           </div>
         </div>
       </div>
     </section>
-    <!-- Section: Design Block -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
